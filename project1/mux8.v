149f2e83c5c2711770d3de85482e74ee7ada8ca6
许 正忞 <Quizas@sandwich.local>
2016-12-03 15:04:48 +0800

    add mux8(8bit)

---
+++ project1/mux8.v
@@ -0,0 +1,28 @@
+module mux8
+(
+control_i,
+select_i,
+control_WB,
+control_MEM,
+control_EX
+);
+
+input	[7:0]	control_i;
+input	select_i;
+output	[1:0]	control_WB, control_MEM;
+output	[3:0]	control_EX;
+
+
+always@(control_i)begin
+	if(select_i == 1'b0) begin
+		control_WB = control_i[7:6];
+		control_MEM = control_i[5:4];
+		control_EX = control_i[3:0];
+	end
+	else begin
+		control_WB = 2'b0;
+		control_MEM = 2'b0;
+		control_EX = 4'b0;
+	end
+end
+endmodule
